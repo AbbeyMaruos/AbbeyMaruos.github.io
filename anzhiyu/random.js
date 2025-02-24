3717ee3c8361225aa61463cbaeb1312bf3d15225
LUOMARCO <luomarcoluo@gmail.com>
2025-02-24 14:44:14 +0800

    Site updated: 2025-02-24 14:44:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/24/测试/"];function toRandomPost(){
+var posts=["2025/02/24/新的一天/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
