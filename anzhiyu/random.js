b6651a1a00d3de7bed47fe46db10f259777864ea
Abbey Maruos <luomarcoluo@gmail.com>
2026-01-11 14:48:19 +0800

    Site updated: 2026-01-11 14:48:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/24/demo1/","2026/01/09/demo4/","2026/01/09/demo2/","2026/01/09/demo3/","2026/01/09/demo5/","2026/01/09/demo6/"];function toRandomPost(){
+var posts=["2025/02/24/demo1/","2026/01/09/demo2/","2026/01/09/demo5/","2026/01/09/demo4/","2026/01/09/demo3/","2026/01/09/demo6/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
